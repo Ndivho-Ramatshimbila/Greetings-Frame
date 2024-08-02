9b9250992c9d31bfcedc7d3a547ad3ae5915a8f9
Ndivho-Ramatshimbila <ndivhoramatshimbila@gmail.com>
2024-08-02 13:16:09 +0200

    Add files via upload

---
+++ GreetingsFrameApp.java
@@ -0,0 +1,19 @@
+package greetingsframeapp;
+
+import za.ac.tut.gui.GreetingsGUI;
+
+/**
+ *
+ * @author ASUS
+ */
+public class GreetingsFrameApp 
+{
+    
+    public static void main(String[] args) 
+    {
+        GreetingsGUI ggui = new GreetingsGUI();
+
+        
+    }
+    
+}
